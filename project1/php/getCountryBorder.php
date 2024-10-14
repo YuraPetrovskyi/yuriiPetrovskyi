8844d7c6855a41c4ce97aecd20d754f66aef75fd
Yurii Petrovskyi <yurakarpaty@gmail.com>
2024-10-14 03:54:13 +0100

    change to English comment

--- project1/php/getCountryBorder.php
+++ project1/php/getCountryBorder.php
@@ -1,8 +1,37 @@
 <?php
-// Вказуємо шлях до файлу countries.geojson
+
+// Connect autoload Composer to load libraries
+require __DIR__ . '/../vendor/autoload.php';
+
+error_log('Failed to load .env file');
+// Loading environment variables from .env
+try {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+} catch (Exception $e) {
+  // Log an error if there is no .env file
+    error_log('Failed to load .env file: ' . $e->getMessage());
+    http_response_code(500);
+    header('Content-Type: application/json');
+    echo json_encode(['error' => 'Failed to load .env file']);
+    exit;
+}
+
+// Check the environment and configure error display
+$environment = $_ENV['ENVIRONMENT'];
+
+if ($environment === 'development') {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+} else {
+    ini_set('display_errors', 0);
+    ini_set('display_startup_errors', 0);
+    error_reporting(0);  // No error reporting in production
+}
+
 $filePath = '../data/countries.geojson';
 
-// Перевіряємо, чи передано isoCode
 if (!isset($_GET['isoCode'])) {
     http_response_code(400);
     echo json_encode(['error' => 'isoCode is required']);
@@ -11,7 +40,6 @@ if (!isset($_GET['isoCode'])) {
 
 $isoCode = $_GET['isoCode'];
 
-// Читаємо файл countries.geojson
 if (!file_exists($filePath)) {
     http_response_code(500);
     echo json_encode(['error' => 'GeoJSON file not found']);
@@ -27,7 +55,6 @@ if (json_last_error() !== JSON_ERROR_NONE) {
     exit;
 }
 
-// Фільтруємо країни за кодом ISO
 $filteredCountry = array_filter($geojsonData['features'], function($feature) use ($isoCode) {
     return $feature['properties']['ISO_A2'] === $isoCode;
 });
@@ -38,7 +65,6 @@ if (empty($filteredCountry)) {
     exit;
 }
 
-// Повертаємо країну у форматі GeoJSON
 $result = [
     'type' => 'FeatureCollection',
     'features' => array_values($filteredCountry)
